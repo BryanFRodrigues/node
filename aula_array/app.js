87dd9d8bd442cd63390653ecfb7599bd177d01cf
Bryan F Rodrigues <93285267+BryanFRodrigues@users.noreply.github.com>
2024-04-22 22:48:43 +0000

    formulario

--- aula_array/app.js
+++ aula_array/app.js
@@ -1,8 +1,23 @@
-const alunos = ['João', 'Maria', 'José', 'Antônio', 'Ana'];
+/*const alunos = ['João', 'Maria', 'José', 'Antônio', 'Ana'];
 
 alunos.push('Carlos');
 alunos.unshift('Mariana');
 
 for(conn of alunos){
     console.log(conn);
+}*/
+
+function criaPessoa(nome,sobrenome,idade){//Construtor de objetos
+    if(typeof nome !== 'string' || typeof sobrenome !== 'string' || typeof idade !== 'number'){
+        throw new Error('Os valores informados não são válidos');   //difernte de java ele precisa ter um if controlando as entradas dos dadoos;
+    }   
+    return{
+        nome : nome,
+        sobrenome : sobrenome,
+        idade : idade
+    };
 }
+
+const pessoa1 = criaPessoa('João', 'Silva','dois');
+
+console.log(pessoa1);
